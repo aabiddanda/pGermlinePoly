f7583b5dc9464fc7f22547fadc60fcfdef09e78a
Arjun Biddanda <aabiddanda@gmail.com>
2023-11-21 13:17:16 -0500

    feat: updated to use cpdef for python importable functions

--- pGermlinePoly/poly_utils.pyx
+++ pGermlinePoly/poly_utils.pyx
@@ -1,13 +1,12 @@
 from libc.math cimport erf, exp, log, pi, sqrt
-import numpy as np
 
 
 cdef double sqrt2 = sqrt(2.);
 cdef double sqrt2pi = sqrt(2*pi);
 cdef double logsqrt2pi = log(1/sqrt2pi)
 
-cdef double logsumexp(double[:] x):
-    """Cython implementation of the logsumexp trick"""
+cpdef double logsumexp(double[:] x):
+    """Cython implementation of the logsumexp trick."""
     cdef int i,n;
     cdef double m = -1e32;
     cdef double c = 0.0;
@@ -18,7 +17,7 @@ cdef double logsumexp(double[:] x):
         c += exp(x[i] - m)
     return m + log(c)
 
-cdef double log_prior(double [:] l, double[:] a):
+cpdef double log_prior(double [:] l, double[:] a):
     """Cython implementation of the logistic function and log-calculation."""
     cdef int i, n;
     cdef double xk;
--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 """Setup module for building pGermlinePoly utilities"""
 
 
+from distutils.core import Extension
+
 from Cython.Build import cythonize
-from setuptools import Extension, setup
+from Cython.Compiler import Options
+from setuptools import setup
 
 extensions = [Extension("poly_utils", ["pGermlinePoly/poly_utils.pyx"])]
 
-setup_args = dict(ext_modules=cythonize(extensions))
+setup_args = dict(
+    ext_modules=cythonize(
+        extensions, compiler_directives={"language_level": 3, "profile": False}
+    )
+)
 setup(**setup_args)
