3785b0905a1f2c3ba933f152f0bfd05459a8257b
Arjun Biddanda <aabiddanda@gmail.com>
2023-11-22 22:32:59 -0500

    draft: method to potentially create new annotations

--- pGermlinePoly/io.py
+++ pGermlinePoly/io.py
@@ -7,17 +7,10 @@ from tqdm import tqdm
 germline_schema = {
     "ind": {"required": True, "type": "string"},
     "sex": {"required": True, "type": "string"},
-    "age": {"required": True, "type": "int"},
-    "germline": {
-        "required": True,
-        "type": "list",
-        "schema": {"value": {"type": "string"}},
-    },
-    "clones": {
-        "required": True,
-        "type": "list",
-        "schema": {"value": {"type": "string"}},
-    },
+    "age": {"required": True, "type": "integer", "min": 0},
+    "germline": {"required": True, "type": "list", "schema": {"type": "string"}},
+    "clones": {"required": True, "type": "list", "schema": {"type": "string"}},
+    "annotations": {"required": True, "type": "list"},
 }
 
 
@@ -25,7 +18,7 @@ def validate_config(config_yaml_fp, schema=germline_schema):
     """Validate a config-file using an underlying schema."""
     v = Validator(schema)
     with open(config_yaml_fp, "r") as stream:
-        config = yaml.load(stream)
+        config = yaml.safe_load(stream)
         assert v.validate(config)
     return config
 
@@ -36,10 +29,10 @@ def check_samples(vcf, samples=[]):
         assert s in vcf.samples
 
 
-def check_anno(vcf, annotations=["PL", "AD"]):
+def check_annotations(vcf, annotations=["PL", "AD"]):
     """Check that the annotations are contained as site-level information as either INFO or FORMAT fields."""
     for a in annotations:
-        assert a in vcf.contains(a)
+        assert vcf.contains(a)
 
 
 def create_germline_anno(vcf):
@@ -60,6 +53,18 @@ def create_germline_anno(vcf):
     return np.array(germline_log_ratio, dtype=np.float32)
 
 
+def create_anno(vcf, annotations=[]):
+    """Extract annotation values from VCF."""
+    total_anno = []
+    for v in tqdm(vcf):
+        if v.is_snp and (len(v.ALT) == 1):
+            anno = [v.INFO[a] for a in annotations]
+        else:
+            anno = [np.nan for a in annotations]
+        total_anno.append(anno)
+    return np.hstack(total_anno)
+
+
 def create_clonal_pl_matrix(vcf):
     """Create the X matrix for inference from clonal samples.
 
@@ -70,7 +75,7 @@ def create_clonal_pl_matrix(vcf):
     X = []
     for v in tqdm(vcf):
         if v.is_snp and (len(v.ALT) == 1):
-            x = v.format("PL")[0]
+            x = v.format("PL")
             X.append(x)
         else:
             # NOTE: we could replace these with NaNs as well to signify missing data...
