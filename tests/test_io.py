f169170d8bafd83df457d17b82f92ec6bd2c5e8d
Arjun Biddanda <aabiddanda@gmail.com>
2023-11-29 12:52:21 -0500

    test: updated some of the config tests

--- tests/test_io.py
+++ tests/test_io.py
@@ -20,6 +20,18 @@ annotations:
   - MLEAF
 """
 
+no_anno_config = """
+ind: JH214UJ
+age: 50
+sex: M
+germline:
+  - JH214UJbulk
+clones:
+  - JH214UJG11
+  - JH214UJG2
+annotations: []
+"""
+
 bad_sex_config = """
 ind: JH214UJ
 age: 50
@@ -33,21 +45,22 @@ annotations:
   - MLEAF
 """
 
-no_anno_config = """
+no_sex_config = """
 ind: JH214UJ
 age: 50
-sex: M
 germline:
   - JH214UJbulk
 clones:
   - JH214UJG11
   - JH214UJG2
-annotations: []
+annotations:
+  - MLEAF
 """
 
 
 def test_valid_config_strings(tmp_path):
-    d = tmp_path / "sub"
+    """Testing valid configurations."""
+    d = tmp_path / "valid"
     d.mkdir()
     p1 = d / "hello.yaml"
     p1.write_text(good_config)
@@ -55,6 +68,17 @@ def test_valid_config_strings(tmp_path):
     p2 = d / "hello2.yaml"
     p2.write_text(no_anno_config)
     validate_config(p2)
+
+
+def test_bad_config_strings(tmp_path):
+    """Testing bad configs in various cases."""
+    d = tmp_path / "invalid"
+    d.mkdir()
+    for c_str in [bad_sex_config, no_sex_config]:
+        p1 = d / "hello.yaml"
+        p1.write_text(c_str)
+        with pytest.raises(Exception):
+            validate_config(p1)
 
 
 # --------- 2. Testing VCFs ---------- #
