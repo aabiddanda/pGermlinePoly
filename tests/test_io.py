de0c8454ea9edb88706e19370a7b0f02f055ba5c
Arjun Biddanda <aabiddanda@gmail.com>
2023-11-30 18:38:57 -0500

    test: updated some clear io tests

--- tests/test_io.py
+++ tests/test_io.py
@@ -220,7 +220,7 @@ def test_validate_vcf(tmp_path):
     check_annotations(cur_vcf, annotations=annotations)
 
 
-def test_annotation_extractions(tmp_path):
+def test_annotation_extraction(tmp_path):
     """Run through a full validation of the VCF protocol."""
     d = tmp_path / "real_vcf_test"
     d.mkdir()
@@ -233,7 +233,36 @@ def test_annotation_extractions(tmp_path):
     annotations = config["annotations"]
     clone_vcf = VCF(vcf_fp, samples=samples)
     X = create_clonal_pl_matrix(clone_vcf)
-    assert X.shape[2] == 2
+    assert X.shape[2] == 3
+    assert X.shape[1] == 2
 
 
-# TODO: write some tests for the annotation tests
+def test_create_germline_anno(tmp_path):
+    """Run through a full validation of the VCF protocol."""
+    d = tmp_path / "real_vcf_test"
+    d.mkdir()
+    config_fp = d / "config.yaml"
+    config_fp.write_text(good_config_real)
+    vcf_fp = d / "test.vcf"
+    vcf_fp.write_text(good_vcf_string)
+    config = validate_config(config_fp)
+    samples = config["germline"]
+    annotations = config["annotations"]
+    germline_vcf = VCF(vcf_fp, samples=samples)
+    germ_anno = create_germline_anno(germline_vcf)
+    assert germ_anno.size > 0
+
+
+def test_create_anno(tmp_path):
+    """Run through a full validation of the VCF protocol."""
+    d = tmp_path / "real_vcf_test"
+    d.mkdir()
+    config_fp = d / "config.yaml"
+    config_fp.write_text(good_config_real)
+    vcf_fp = d / "test.vcf"
+    vcf_fp.write_text(good_vcf_string)
+    config = validate_config(config_fp)
+    samples = config["germline"]
+    annotations = config["annotations"]
+    germline_vcf = VCF(vcf_fp, samples=samples)
+    anno = create_anno(germline_vcf, annotations=annotations)
